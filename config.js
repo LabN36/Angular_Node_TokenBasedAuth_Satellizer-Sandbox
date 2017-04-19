94c45c2e952ae0d28183ab11cdf9749676409294
Rishabh Agrawal <ragrawal804@gmail.com>
2017-04-19 14:05:19 +0530

    working commit

---
+++ config.js
@@ -0,0 +1,13 @@
+module.exports = {
+  // App Settings
+  MONGO_URI: process.env.MONGO_URI || 'mongodb://test:test@ds049744.mlab.com:49744/github', //'localhost',
+  TOKEN_SECRET: process.env.TOKEN_SECRET || 'YOUR_UNIQUE_JWT_TOKEN_SECRET',
+// mongodb://<dbuser>:<dbpassword>@ds019980.mlab.com:19980/ipl16
+  // OAuth 2.0
+  FACEBOOK_SECRET: process.env.FACEBOOK_SECRET || 'b5d2e905b602e8e2a9a43e1ea2d2f1ef',
+  GOOGLE_SECRET: process.env.GOOGLE_SECRET || 'YOUR_GOOGLE_CLIENT_SECRET',
+
+  // OAuth 1.0
+  TWITTER_KEY: process.env.TWITTER_KEY || 'vdrg4sqxyTPSRdJHKu4UVVdeD',
+  TWITTER_SECRET: process.env.TWITTER_SECRET || 'cUIobhRgRlXsFyObUMg3tBq56EgGSwabmcavQP4fncABvotRMA'
+};
